df6a906c98fdf029b396621fba4839247286e5af
Tony Schneider <tonywok@gmail.com>
2014-03-06 15:24:11 -0500

    Spike over, tests incoming

--- lib/generators/templates/models/domain_model.rb
+++ lib/generators/templates/models/domain_model.rb
@@ -2,7 +2,4 @@ class <%= domain_model_class %> < ActiveRecord::Base
 
   has_many :<%= plural_identity_model %>, inverse_of: :<%= domain_model %>
 
-  validates_presence_of :email
-  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-
 end
--- lib/generators/templates/models/identity_model.rb
+++ lib/generators/templates/models/identity_model.rb
@@ -1,9 +1,11 @@
 class <%= identity_model_class %> < ActiveRecord::Base
 
-  has_secure_password
-
   include TokenVerification
 
   belongs_to :<%= domain_model %>, inverse_of: :<%= plural_identity_model %>
+
+  has_secure_password
+  validates_presence_of :email
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
 end
