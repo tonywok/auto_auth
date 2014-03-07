0565000551a280b024c0755512ace5584803a7dd
Tony Schneider <tonywok@gmail.com>
2014-03-06 20:49:52 -0500

    Remove user confirmation, fix erb typo

--- lib/generators/templates/models/concerns/token_verification.rb
+++ lib/generators/templates/models/concerns/token_verification.rb
@@ -5,17 +5,11 @@ module TokenVerification
   class ExpiredToken < StandardError; end
 
   PASSWORD_RESET = 'password_reset'
-  EMAIL_CONFIRMATION = 'email_confirmation'
 
   extend ActiveSupport::Concern
 
   def password_reset_token
     verifier = self.class.verifier_for(PASSWORD_RESET)
-    verifier.generate([id, Time.now])
-  end
-
-  def email_confirmation_token
-    verifier = self.class.verifier_for(EMAIL_CONFIRMATION)
     verifier.generate([id, Time.now])
   end
 
@@ -32,13 +26,7 @@ module TokenVerification
     def find_by_password_reset_token!(token)
       <%= domain_model_id %>, timestamp = self.class.verifier_for(PASSWORD_RESET).verify(token)
       raise ::TokenVerification::ExpiredToken if timestamp < 1.day.ago
-      <% identity_model_class %>.find(<%= domain_model_id %>)
-    end
-
-    def find_by_email_confirmation!(token)
-      <%= domain_model_id %>, timestamp = self.class.verifier_for(EMAIL_CONFIRMATION).verify(token)
-      raise ::TokenVerification::ExpiredToken if timestamp < 1.day.ago
-      <% identity_model_class %>.find(<%= domain_model_id %>)
+      <%= identity_model_class %>.find(<%= domain_model_id %>)
     end
 
   end
